3e2a07a1520d79ef18dae1ef8ecdc3bdda04ad39
Victor Refosco <victorroatt6@hotmail.com>
2025-05-22 14:26:45 -0300

    Ajuste run aws sync Ajuste politica do bucket

--- infra/main.tf
+++ infra/main.tf
@@ -25,15 +25,11 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
+        Sid       = "AllowPublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
-        Action    = [
-          "s3:GetObject"
-        ]
-        Resource = [
-          "${aws_s3_bucket.static_site.arn}/*"
-        ]
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
       }
     ]
   })
